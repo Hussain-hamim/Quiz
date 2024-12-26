e68667c1e157ab3880ca39bfda8bf5e64e4bd26a
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-12-26 19:51:16 +0430

    all done

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import QuizScreen from "./src/app/QuizScreen";
+import QuizProvider from "./src/providers/QuizProvider";
+
+export default function App() {
+  return (
+    <>
+      <QuizProvider>
+        <QuizScreen />
+      </QuizProvider>
+
+      <StatusBar style="auto" />
+    </>
+  );
+}
