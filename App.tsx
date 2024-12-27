f442cf0104c9ec89303c5837646c93175c174d4d
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-12-27 12:06:57 +0430

    add start screen

--- App.tsx
+++ App.tsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import QuizScreen from "./src/app/QuizScreen";
 import QuizProvider from "./src/providers/QuizProvider";
+import CustomButton from "./src/components/CustomButton";
+import { ImageBackground, View } from "react-native";
 
 export default function App() {
+  const [start, setstart] = useState(true);
+
   return (
     <>
       <QuizProvider>
-        <QuizScreen />
+        {start ? (
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "lightblue",
+            }}
+          >
+            {/* <ImageBackground source={require("./assets/background.jpg")}> */}
+            <CustomButton
+              title="Start the game"
+              onPress={() => setstart(false)}
+            />
+            {/* </ImageBackground> */}
+          </View>
+        ) : (
+          <QuizScreen />
+        )}
       </QuizProvider>
 
       <StatusBar style="auto" />
